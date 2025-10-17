da51755b2da29200d3e477392332defda48df0fe
Alex-S-09 <149537459+Alex-S-09@users.noreply.github.com>
2025-10-16 23:36:06 -0500

    Starting Limelight Framework

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/Limelight.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.Sensors;
 
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Limelight {
+
+    private Limelight3A limelight;
+
+    public Limelight(HardwareMap hardwareMap, int pipeline) {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(pipeline);
+    }
+
+    public void switchPipeline(int pipeline) {
+        limelight.pipelineSwitch(pipeline);
+    }
+
+    public void pause() {
+        limelight.pause();
+    }
+
+    public void start() {
+        limelight.start();
+    }
+
+    public void stop(){
+        limelight.stop();
+    }
 }
