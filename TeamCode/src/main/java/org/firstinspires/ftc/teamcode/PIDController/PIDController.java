05a96fc5a50ed1bc1e4fba76c563e50b459ebe9b
Alex-S-09 <149537459+Alex-S-09@users.noreply.github.com>
2025-11-10 22:49:00 -0600

    PID Controller Creation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController/PIDController.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.PIDController;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class PIDController {
+    private final VoltageSensor batteryVoltageSensor;
+    private double kp, ki, kd, integralSum, lastError;
+    private final double targetVoltage = 13.75;
+
+    ElapsedTime timer = new ElapsedTime();
+
+    public PIDController(double kp, double ki, double kd, HardwareMap hardwareMap) {
+        this.kp = kp;
+        this.ki = ki;
+        this.kd = kd;
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
+    }
+
+    //Returns a value from 0 to 1; the returned value will be the power the motors are set to
+    public double controlVoltage() {
+        double currentVoltage = batteryVoltageSensor.getVoltage();
+        double error = targetVoltage - currentVoltage;
+        integralSum += error * timer.seconds();
+        double derivative = (error - lastError) / timer.seconds();
+
+        double output = (error * kp) + (derivative * kd) + (integralSum * ki);
+        output = Math.max(0, Math.min(1, output));
+
+        lastError = error;
+        timer.reset();
+
+        return output;
+    }
+}
