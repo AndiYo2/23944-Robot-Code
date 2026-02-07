c14c9626b7df647b76e9d122344b1f6748375792
AndiYo2 <andyi2@icloud.com>
2026-02-06 20:54:33 -0600

    Flipper fix (again)

--- TeamCode/src/main/java/Constants/ShooterConstants.java
+++ TeamCode/src/main/java/Constants/ShooterConstants.java
@@ -9,7 +9,7 @@ import com.bylazar.configurables.annotations.Configurable;
 public class ShooterConstants {
     // ==================== FLIPPER POSITIONS ====================
     public static double FLIPPER_POSITION_EXTENDED = 0.25;
-    public static double FLIPPER_POSITION_RETRACT = 0.45;
+    public static double FLIPPER_POSITION_RETRACT = 0.46;
     public static double SHOOTER_FLICK_TIME = 0.2;
 
     // ==================== VELOCITY CONSTANTS ====================
