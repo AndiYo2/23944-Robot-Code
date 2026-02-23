9a2bd0bf5d93c13437772750bd828e77ea383333
AndiYo2 <andyi2@icloud.com>
2026-02-22 20:20:01 -0600

    Fixed 17 Ball

--- TeamCode/src/main/java/Autos/EighteenBalls/Red18Ball.java
+++ TeamCode/src/main/java/Autos/EighteenBalls/Red18Ball.java
@@ -11,36 +11,39 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import commands.CommandSequenceBuilder;
 
 
-@Autonomous(name = "\"Working\"Red18Ball")
+@Autonomous(name = "Red18Ball")
 public class Red18Ball extends AutonTemplate {
     public static double maxSpeed = 1;
-    private PathChain startToShoot, shootToFirst, firstToShoot, ShootToGate1, GateToShoot1, ShootToGate2, GateToShoot2, ShootToFourth, fourthToShoot, shootToFifth, fifthToShootAndStop, testingStop;
+    private PathChain startToShoot, shootToFirst, firstToShoot, shootToGatePrep1, prepToThree, gateToShoot1, shootToGatePrep2, path13, gateToShoot2, shootToFourth, fourthToShoot, shootToFifth, fifthToShootAndStop, testingStop;
 
     // Named pose constants
-    private final Pose startPose = new Pose(111.2500, 132.00, Math.toRadians(90)); //DO NOT CHANGE EVEN IF THE POINTS I PASS IN ARE DIFFERENT
-    private final Pose shootPose = new Pose(88.000, 82.500, Math.toRadians(45));
+    private final Pose startPose = new Pose(111.250, 135.000, Math.toRadians(90));
+    private final Pose shootPose = new Pose(88.000, 85.500, Math.toRadians(90));
 
     // ShootToFirst path
-    private final Pose firstControlPoint = new Pose(78, 54.5);
-    private final Pose firstPickupPose = new Pose(123.500, 52.500, Math.toRadians(0));
+    private final Pose firstControlPoint = new Pose(78.000, 57.500);
+    private final Pose firstPickupPose = new Pose(123.500, 55.500, Math.toRadians(0));
 
     // Main shoot position
-    private final Pose mainShootPose = new Pose(86.000, 79.000, Math.toRadians(45));
+    private final Pose mainShootPose = new Pose(86.000, 82.000, Math.toRadians(45));
 
     // Gate paths
-    private final Pose gatePose = new Pose(132.000, 57, Math.toRadians(30));
-    private final Pose gateShootPose = new Pose(86.000, 79.000, Math.toRadians(30));
+    private final Pose gatePrepControlPoint = new Pose(102.000, 68.500);
+    private final Pose gateControlPoint = new Pose(108.000, 65.000);
+    private final Pose gatePrepPose = new Pose(128.000, 63.000, Math.toRadians(0));
+    private final Pose gateInnerControlPoint = new Pose(125, 57.500);
+    private final Pose gatePose = new Pose(132.500, 55.000, Math.toRadians(10));
+    private final Pose gateShootPose = new Pose(86.000, 82.000, Math.toRadians(30));
 
     // ShootToFourth path
-    private final Pose fourthControlPoint = new Pose(107.500, 77.000);
-    private final Pose fourthPickupPose = new Pose(127.000, 75.500, Math.toRadians(0));
+    private final Pose fourthPickupPose = new Pose(127.000, 82.000, Math.toRadians(0));
 
     // ShootToFifth path
-    private final Pose fifthControlPoint = new Pose(81.500, 24.500);
-    private final Pose fifthPickupPose = new Pose(125.000, 29.000, Math.toRadians(0));
+    private final Pose fifthControlPoint = new Pose(85.000, 27.500);
+    private final Pose fifthPickupPose = new Pose(125.000, 32.000, Math.toRadians(0));
 
     // Stop
-    private final Pose stopPose = new Pose(92.000, 109.500, Math.toRadians(45));
+    private final Pose stopPose = new Pose(92.000, 112.500, Math.toRadians(45));
 
     @Override
     protected void buildPaths() {
@@ -64,32 +67,44 @@ public class Red18Ball extends AutonTemplate {
                 .setGlobalDeceleration()
                 .build();
 
-        ShootToGate1 = follower.pathBuilder()
-                .addPath(new BezierLine(gateShootPose, gatePose))
-                .setLinearHeadingInterpolation(gateShootPose.getHeading(), gatePose.getHeading())
+        shootToGatePrep1 = follower.pathBuilder()
+                .addPath(new BezierCurve(gateShootPose, gatePrepControlPoint, gatePrepPose))
+                .setLinearHeadingInterpolation(gateShootPose.getHeading(), gatePrepPose.getHeading())
                 .setGlobalDeceleration()
                 .build();
 
-        GateToShoot1 = follower.pathBuilder()
-                .addPath(new BezierLine(gatePose, gateShootPose))
+        prepToThree = follower.pathBuilder()
+                .addPath(new BezierCurve(gatePrepPose, gateInnerControlPoint, gatePose))
+                .setLinearHeadingInterpolation(gatePrepPose.getHeading(), gatePose.getHeading())
+                .setGlobalDeceleration()
+                .build();
+
+        gateToShoot1 = follower.pathBuilder()
+                .addPath(new BezierCurve(gatePose, gateControlPoint, gateShootPose))
                 .setLinearHeadingInterpolation(gatePose.getHeading(), gateShootPose.getHeading())
                 .setGlobalDeceleration()
                 .build();
 
-        ShootToGate2 = follower.pathBuilder()
-                .addPath(new BezierLine(gateShootPose, gatePose))
-                .setLinearHeadingInterpolation(gateShootPose.getHeading(), gatePose.getHeading())
+        shootToGatePrep2 = follower.pathBuilder()
+                .addPath(new BezierCurve(gateShootPose, gatePrepControlPoint, gatePrepPose))
+                .setLinearHeadingInterpolation(gateShootPose.getHeading(), gatePrepPose.getHeading())
                 .setGlobalDeceleration()
                 .build();
 
-        GateToShoot2 = follower.pathBuilder()
-                .addPath(new BezierLine(gatePose, gateShootPose))
+        path13 = follower.pathBuilder()
+                .addPath(new BezierCurve(gatePrepPose, gateInnerControlPoint, gatePose))
+                .setLinearHeadingInterpolation(gatePrepPose.getHeading(), gatePose.getHeading())
+                .setGlobalDeceleration()
+                .build();
+
+        gateToShoot2 = follower.pathBuilder()
+                .addPath(new BezierCurve(gatePose, gateControlPoint, gateShootPose))
                 .setLinearHeadingInterpolation(gatePose.getHeading(), gateShootPose.getHeading())
                 .setGlobalDeceleration()
                 .build();
 
-        ShootToFourth = follower.pathBuilder()
-                .addPath(new BezierCurve(gateShootPose, fourthControlPoint, fourthPickupPose))
+        shootToFourth = follower.pathBuilder()
+                .addPath(new BezierLine(gateShootPose, fourthPickupPose))
                 .setLinearHeadingInterpolation(gateShootPose.getHeading(), fourthPickupPose.getHeading())
                 .setGlobalDeceleration()
                 .build();
@@ -121,21 +136,48 @@ public class Red18Ball extends AutonTemplate {
     @Override
     public void init() {
         super.init();
-        SpindexerConstants.currentMode = EnumConstants.ShootingMode.Sorted;
+        SpindexerConstants.currentMode = EnumConstants.ShootingMode.Fast;
         Constants.RobotConstants.Robot.allianceColor = Constants.EnumConstants.AllianceColor.Red;
 
         autonomousCommand = new CommandSequenceBuilder(follower, intake, spindexer, limelight, shooter, turret)
+                .setShootWhileMoving(true)
                 .moveTo(startToShoot, maxSpeed, true)
+                .shoot()
+                .limelightScan()
+                .setShootWhileMoving(false)
+                .intakeStart()
                 .moveTo(shootToFirst, maxSpeed, true)
+                .autoCatalog()
                 .moveTo(firstToShoot, maxSpeed, true)
-                .moveTo(ShootToGate1, .5, true)
-                .moveTo(GateToShoot1, maxSpeed, true)
-                .moveTo(ShootToGate2, .5, true)
-                .moveTo(GateToShoot2, maxSpeed, true)
-                .moveTo(ShootToFourth, maxSpeed, true)
+                .shoot()
+                .moveTo(shootToGatePrep1, .7, true)
+                .delay(.02)
+                .intakeStart()
+                .moveTo(prepToThree, 1, true)
+                .delay(1)
+                .autoCatalog()
+                .moveTo(gateToShoot1, maxSpeed, true)
+                .shoot()
+                .setSpindexerMode(EnumConstants.ShootingMode.Sorted)
+                .delay(.5)
+                .moveTo(shootToGatePrep2, .5, true)
+                .delay(.02)
+                .intakeStart()
+                .moveTo(path13, 1, true)
+                .delay(1)
+                .autoCatalog()
+                .moveTo(gateToShoot2, maxSpeed, true)
+                .shoot()
+                .intakeStart()
+                .moveTo(shootToFourth, maxSpeed, true)
+                .autoCatalog()
                 .moveTo(fourthToShoot, maxSpeed, true)
+                .shoot()
+                .intakeStart()
                 .moveTo(shootToFifth, maxSpeed, true)
+                .autoCatalog()
                 .moveTo(fifthToShootAndStop, maxSpeed, true)
+                .shoot()
                 .moveTo(testingStop, maxSpeed, true)
                 .build();
     }
