0dd3fd1c3d8628b3eb619d1d63bcff941930c198
AndiYo2 <andyi2@icloud.com>
2026-03-04 20:35:03 -0600

    BackAutosDone

--- TeamCode/src/main/java/Autos/FifteenBalls/BlueBackCycler.java
+++ TeamCode/src/main/java/Autos/FifteenBalls/BlueBackCycler.java
@@ -11,39 +11,39 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import commands.CommandSequenceBuilder;
 
 
-@Autonomous(name = "RedBackCycler")
-public class RedBackCycler extends AutonTemplate {
+@Autonomous(name = "BlueBackCycler")
+public class BlueBackCycler extends AutonTemplate {
     public static double maxSpeed = 1;
     private PathChain shootToFirst, firstToShoot, shootToSecond, secondToShoot, shootToThird, thirdToShoot, shootToFourth, fourthToShoot, shootToFifth, fifthToShoot, shootToStop;
 
     // Start pose
-    private final Pose startPose = new Pose(87.800, 8.000, Math.toRadians(90));
+    private final Pose startPose = new Pose(56.200, 8.000, Math.toRadians(90));
 
     // Start curve control point
-    private final Pose startCurveControl = new Pose(91.000, 17.000);
+    private final Pose startCurveControl = new Pose(53.000, 17.000);
 
     // Second pickup (upper row)
-    private final Pose secondCurveControl = new Pose(97.500, 39.000);
-    private final Pose secondPickupPose = new Pose(130.000, 34.000, Math.toRadians(0));
+    private final Pose secondCurveControl = new Pose(50.000, 40.000);
+    private final Pose secondPickupPose = new Pose(14.000, 37.000, Math.toRadians(180));
 
     // Shoot position
-    private final Pose shootPose = new Pose(89.000, 16.000, Math.toRadians(30));
-    private final Pose postShootPose = new Pose(89.000, 16.000, Math.toRadians(0));
+    private final Pose shootPose = new Pose(55.000, 16.000, Math.toRadians(150));
+    private final Pose postShootPose = new Pose(55.000, 16.000, Math.toRadians(180));
 
     // Cycle prep & pickup
-    private final Pose cyclePrepPose = new Pose(105.000, 11.500, Math.toRadians(0));
-    private final Pose cyclePrepPoseAngled = new Pose(105.000, 11.500, Math.toRadians(-10));
-    private final Pose firstCyclePickupPose = new Pose(131.500, 11.500, Math.toRadians(0));
-    private final Pose cyclePickupPose = new Pose(131.000, 11.500, Math.toRadians(0));
+    private final Pose cyclePrepPose = new Pose(39.000, 11.500, Math.toRadians(180));
+    private final Pose cyclePrepPoseAngled = new Pose(39.000, 11.500, Math.toRadians(190));
+    private final Pose firstCyclePickupPose = new Pose(13.000, 11.500, Math.toRadians(180));
+    private final Pose cyclePickupPose = new Pose(14.000, 11.500, Math.toRadians(180));
 
     // Return to shoot control point
-    private final Pose returnToShootControl = new Pose(111.000, 19.000);
+    private final Pose returnToShootControl = new Pose(33.000, 19.000);
 
     // Fourth pickup
-    private final Pose fourthPickupPose = new Pose(130.000, 23.000, Math.toRadians(0));
+    private final Pose fourthPickupPose = new Pose(14.000, 23.000, Math.toRadians(180));
 
     // End pose
-    private final Pose stopPose = new Pose(94.500, 20.500, Math.toRadians(30));
+    private final Pose stopPose = new Pose(49.500, 20.500, Math.toRadians(150));
 
     @Override
     protected void buildPaths() {
@@ -115,7 +115,7 @@ public class RedBackCycler extends AutonTemplate {
     public void init() {
         super.init();
         SpindexerConstants.currentMode = EnumConstants.ShootingMode.Fast;
-        Constants.RobotConstants.Robot.allianceColor = Constants.EnumConstants.AllianceColor.Red;
+        Constants.RobotConstants.Robot.allianceColor = Constants.EnumConstants.AllianceColor.Blue;
 
         autonomousCommand = new CommandSequenceBuilder(follower, intake, spindexer, limelight, shooter, turret)
                 .delay(.35)
